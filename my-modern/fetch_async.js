a21402468da229ec77199f03feec0f8897335805
shinji.fujita <shinji.fujita@g.dreamcareer.co.jp>
2026-05-29 19:45:00 +0900

    5.29 Reactアプリ

--- my-modern/fetch_async.js
+++ my-modern/fetch_async.js
@@ -1,10 +1,12 @@
 async function fetchData() {
-  // const response = await fetch('https://wings.msn.to/tmp/it/fetch.php');
-  const response = fetch('https://wings.msn.to/tmp/it/fetch.php');
+  const response = await fetch('https://wings.msn.to/tmp/it/fetch.php');
+  // const response = fetch('https://wings.msn.to/tmp/it/fetch.php');
   if (!response.ok) { throw new Error(response.statusText); }
-  // const text = await response.text();
-  const text = response.text();
+  // 修正ポイント：await をつけて、テキスト化の完了を待つ
+  const text = await response.text();
+  // const text = response.text();
   console.log(text);
 }
 
 fetchData();
+console.log('test111');
